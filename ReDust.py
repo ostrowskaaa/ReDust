17782094f4bcb1b6896607698032f458165e00c8
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2020-04-23 13:42:20 +0200

    Add files via upload

---
+++ ReDust.py
@@ -0,0 +1,63 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.preprocessing.image import img_to_array, array_to_img
+
+
+def LoadColorData(folderName):
+    PATH = os.getcwd()
+    folder_path = PATH + '/data/' + folderName + '/'
+    data = os.listdir(folder_path)
+    x_data = []
+    for sample in data:
+        img_path = folder_path + sample
+        x = cv2.imread(img_path,1)
+        # rescale so there are only values between 0 and 1
+        x = x / 255.0
+        x_data.append(x)
+
+    x_data = np.array(x_data)
+    return x_data
+
+def LoadBWData(folderName):
+    PATH = os.getcwd()
+    folder_path = PATH + '/data/' + folderName + '/'
+    data = os.listdir(folder_path)
+    x_data = []
+    for sample in data:
+        img_path = folder_path + sample
+        x = cv2.imread(img_path, 0)
+        x_data.append(x)
+
+    x_data = np.array(x_data)
+    return x_data
+
+# load training data
+train = 'train'
+train_images = LoadColorData(train)
+
+# load training masks
+train_mask = 'train_mask'
+train_mask = LoadBWData(train_mask)
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(100,100,3)),
+    keras.layers.Dense(32, activation='sigmoid'),
+    keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+model.fit(train_images, train_mask, batch_size=32, epochs=1)
+
+eval_model=classifier.evaluate(train_images, train_mask, verbose=2)
+
+print('Test accuracy:', eval_model)
---
+++ cvPart.py
@@ -0,0 +1,36 @@
+from cv2 import cv2
+import numpy as np
+
+
+# ---- IMAGE COMPRARISON ----
+img1 = cv2.imread('kot1.jpg')
+img2 = cv2.imread('kot1a.jpg')
+
+if img1.shape == img2.shape:
+    difference = cv2.subtract(img1, img2)
+    # if the whole image is black -> there is no difference between images
+    r, g, b = cv2.split(difference)
+    if cv2.countNonZero(r) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(b) == 0:
+       print("images are equal")
+    else:
+        print("images are not equal")
+        # percentage difference
+        percentage = (np.count_nonzero(difference)*100) / difference.size
+        print("they are different in: ", percentage, "%")
+else:
+    print("images are not the same size")
+
+
+# ---- IMAGE INPAINTING ----
+img = cv2.imread('1.jpg')
+mask = cv2.imread('1-mask.jpg', 0)
+
+repaired = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+
+# save output
+cv2.imwrite('repaired1.jpg', repaired)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
