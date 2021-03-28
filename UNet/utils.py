02494f7f9a522de9350b01e2500b5d2b12c52388
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2021-03-29 01:15:06 +0200

    Update utils.py

--- UNet/utils.py
+++ UNet/utils.py
@@ -1,6 +1,13 @@
 import os
 import numpy as np
 from cv2 import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+
+PATH = os.getcwd()
 
 ## ----- LOAD DATA ------
 def load_data(folder_name, photo_type = 1):
@@ -33,3 +40,21 @@ def dataset_transformation(images_array):
             rotated_img = cv2.warpAffine(image, matrix, (width, height))
             dataset.append(rotated_img)
     return np.array(dataset)
+
+def plot_acc_loss(accuracy_values, loss_values, saving_name):
+    # plot training ACCURACY VALUES
+    fig = plt.figure()
+    gs = fig.add_gridspec(ncols = 1,nrows = 2)
+    plt.subplot(gs[0])
+    plt.plot(accuracy_values)
+    plt.ylabel('Accuracy')
+    plt.legend(['Train data'], loc = 'upper left')
+
+    # plot training LOSS VALUES
+    plt.subplot(gs[1])
+    plt.plot(loss_values)
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train data'], loc = 'upper left')
+    plt.tight_layout()
+    fig.savefig(os.path.join(PATH, 'results','') + '{}.png'.format(saving_name), dpi = fig.dpi)
